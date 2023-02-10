6cad8e4239abeda5c53ff96da7abad2214765666
EricBorges1 <ericborgeslg3@gmail.com>
2023-02-09 21:36:26 -0300

    Aula de strings, avante !

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,31 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringSemEspaco = "Eu não sou supersticioso, mas sou um pouco ________.";
+console.log(minhaString.length);
+console.log(minhaString.trim());
+console.log(minhaStringSemEspaco.length);
+console.log(minhaString.replaceAll("________","sticioso")); 
+
+/* const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringSemEspacos = minhaString.trim();
+
+function retornarSubstiuição(fraseInicial) {
+    
+    const fraseFinal = fraseInicial.replace('________','sticioso');
+    console.log(`A frase após a substituição é: ${fraseFinal}`);
+}
+
+retornarSubstiuição(minhaStringSemEspacos); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
